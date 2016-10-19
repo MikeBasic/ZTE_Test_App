1d02418641403566166efe221c070b94d7d0f1bb
MikeBasic <zhemintu@gmail.com>
2016-10-18 23:16:15 -0500

    Add the function to read a text file from phone. It does not exit gracefully after "end" is typed though.

--- app/src/main/java/com/tu/zhemin/zte_test_app/DisplayMessageActivity.java
+++ app/src/main/java/com/tu/zhemin/zte_test_app/DisplayMessageActivity.java
@@ -1,13 +1,13 @@
 package com.tu.zhemin.zte_test_app;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -16,19 +16,34 @@ public class DisplayMessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
+        String fLine=null;
+        String sIn;
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(message);
 
+        // read file and display
+        try {
+            while ((sIn = MainActivity.bufReader.readLine()) != null) {
+
+                fLine += (sIn + "\n");
+
+                // System.out.println(fline);
+//                String[] strs = fLine.trim().split(" ");
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(10);
+        textView.setText(message + "\n" + fLine);
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
         if (layout != null) {
             layout.addView(textView);
         }
 
-        if (message.compareTo("end") == 0)
-        {
+        if (message.compareTo("end") == 0) {
             DisplayMessageActivity.this.finishAffinity();
             System.exit(0);
         }
